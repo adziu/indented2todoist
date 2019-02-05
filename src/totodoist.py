eadaed86130d5024b74c10bca07535d049823c84
Adrian Dziubek <adrian.dziubek@gmail.com>
2019-02-05 16:36:09 +0100

    Added string constants for CSV generation.

---
+++ src/totodoist.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+HEADER = (
+    'TYPE,CONTENT,PRIORITY,INDENT,AUTHOR,RESPONSIBLE,DATE,DATE_LANG,TIMEZONE'
+)
+USER = 'Adrian (21609785)'
+DEFAULTS = 'task,{content},4,1,{USER},,,en,Europe/Warsaw'
