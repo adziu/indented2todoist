bbacbee1386c9649f1ab3589375b28710b4bb171
Adrian Dziubek <adrian.dziubek@gmail.com>
2019-02-05 17:38:23 +0100

    Added _strip_header_and_initial_indent.

--- src/tests/test_indented.py
+++ src/tests/test_indented.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+import totodoist
+
+
+def test_strip_header_and_initial_indent():
+    input_lines = '''\
+task_list_name
+     task_not_indented
+         task_indented'''.split('\n')
+    expected = '''\
+task_not_indented
+    task_indented'''.split('\n')
+    result = list(totodoist._strip_header_and_initial_indent(input_lines))
+    assert result == expected
--- src/totodoist.py
+++ src/totodoist.py
@@ -3,23 +3,35 @@
 
 import click
 
+@click.command(
+    help='Convert indented text files to CSV',
+)
+@click.argument(
+    'indented',
+    nargs=-1,
+    type=click.File(),
+)
+def indented_command(indented):
+    """Convert indented *.txt files into *.csv for Todoist."""
+    for indented_file in indented:
+        tasks = _indented_tasks(indented_file)
+
+
 # input
 INITIAL_INDENT = ' ' * 5
 INDENT = ' ' * 4
 
 
-@click.command(
-    help='Indented text file paths to convert to CSV',
-)
-@click.argument(
-    'indented_text',
-    nargs=-1,
-    type=click.File(),
-)
-def indented(indented_text):
-    """Convert indented *.txt files into *.csv for Todoist."""
-    for f in indented_text:
-        print(f, f.name)
+def _indented_tasks(lines):
+    yield from _strip_header_and_initial_indent(lines)
+
+
+def _strip_header_and_initial_indent(lines):
+    lines_iter = iter(lines)
+    next(lines_iter, None)
+    for line in lines_iter:
+        without_initial = line[len(INITIAL_INDENT):]
+        yield without_initial
 
 
 # output
@@ -32,4 +44,4 @@ DEFAULTS = 'task,{content},4,1,{USER},,,en,Europe/Warsaw'
 
 # click entry point
 if __name__ == '__main__':
-    indented()
+    indented_command()
